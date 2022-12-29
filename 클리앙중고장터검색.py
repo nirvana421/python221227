2a0890f3eee8f0060a56a5114203e4308f4daf6a
nirvana421 <nirvana421@naver.com>
2022-12-29 16:20:36 +0900

    클리앙 사이트 연습

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -9,20 +9,30 @@ hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) Appl
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
         data = urllib.request.urlopen(req).read()
+        # 한글이 깨지는 경우는 다시 해석
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.findAll('a', attrs={'class':'list_subject'})
+        # <span> 태그에서 필터링(attributes)
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+
+# <span class="subject_fixed" data-role="list-title-text" title="[정리] 윈도우 시스템 관리 팁 목록">
+# 							[정리] 윈도우 시스템 관리 팁 목록
+# </span>
 
         for item in list:
                 try:
                         #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
+                        # span = item.contents[1]
+                        # span2 = span.nextSibling.nextSibling
+                        # title = span2.text 
+                        title = item.text.strip()
+                        # print(title)
+                        # 정규표현식
                         if (re.search('아이폰', title)):
                                 print(title.strip())
                                 print('https://www.clien.net'  + item['href'])
